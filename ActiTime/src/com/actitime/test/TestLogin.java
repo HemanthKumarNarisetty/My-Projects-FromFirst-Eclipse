5e34f92050fe1e2d7771f69b1e4ccc1ec147e3df
TYSS <TYSS@TYSS>
2021-11-19 18:45:17 +0530

    updated second actitime project

---
+++ ActiTime/src/com/actitime/test/TestLogin.java
@@ -0,0 +1,14 @@
+package com.actitime.test;
+
+import org.testng.annotations.Test;
+
+import com.actitime.generics.Base;
+
+public class TestLogin extends Base {
+
+	@Test
+	public void loginTesting()
+	{
+		
+	}
+}
